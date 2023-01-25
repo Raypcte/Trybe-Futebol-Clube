578695e15e468f6b4039d732bd75fff394de215e
Raypcte <rayanevalak@gmail.com>
2023-01-24 23:03:07 -0300

    Configurando o docker e o model

---
+++ app/backend/src/database/models/User.ts
@@ -0,0 +1,53 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+// import OtherModel from './OtherModel';
+
+class User extends Model {
+  declare id: number;
+  declare username: string;
+  declare email: string;
+  declare password: string;
+  declare role: string;
+}
+
+User.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  username: {
+    type: STRING,
+    allowNull: false,
+  },
+  email: {
+    type: STRING,
+    allowNull: false,
+  },
+  password: {
+    type: STRING,
+    allowNull: false,
+  },
+  role: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'users',
+  timestamps: false,
+});
+
+/**
+  * `Workaround` para aplicar as associations em TS:
+  * Associations 1:N devem ficar em uma das instâncias de modelo
+  * */
+
+// OtherModel.belongsTo(User, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
+// OtherModel.belongsTo(User, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
+
+// User.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
+// User.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
+
+export default User;
