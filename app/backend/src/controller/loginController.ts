4a38496e7fc9f6759c320c459224960e531bdc22
Raypcte <rayanevalak@gmail.com>
2023-01-28 19:55:59 -0300

    validando email e senha corretos

--- app/backend/src/controller/loginController.ts
+++ app/backend/src/controller/loginController.ts
@@ -6,6 +6,13 @@ import loginService from '../services/loginService';
 const login = async (req: Request | any, res: Response) => {
   const { email, password } = req.body;
   const user: User | null = await loginService({ email, password });
+
+  if (!user) {
+    return res.status(401).json(
+      { message: 'Incorrect email or password' },
+    );
+  }
+
   const token = createToken(user);
   req.token = token;
   req.userId = user?.id;
