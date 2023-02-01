9337129aba1e34d0f81faae78e3e3150ab927d17
Raypcte <rayanevalak@gmail.com>
2023-01-31 23:08:17 -0300

    req 21 e 22

--- app/backend/src/controller/matchesController.ts
+++ app/backend/src/controller/matchesController.ts
@@ -1,8 +1,18 @@
 import { Request, Response } from 'express';
 import getMatchesServices from '../services/matchesService';
 
-const getMatches = async (_req: Request, res: Response) => {
+const getMatches = async (req: Request, res: Response) => {
+  const { inProgress }: any = req.query;
   const matches = await getMatchesServices();
+  if (inProgress === 'true') {
+    const filterMatches = matches.filter((i) => i.inProgress === true);
+    return res.status(200).json(filterMatches);
+  }
+  if (inProgress === 'false') {
+    const filterMatches = matches.filter((i) => i.inProgress === false);
+    return res.status(200).json(filterMatches);
+  }
+
   return res.status(200).json(matches);
 };
 
